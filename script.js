54b33551cfb26e979ba9d64fadee6031aa61fdf2
nkoubamarius <nkoubamarius@gmail.com>
2020-11-18 12:27:45 +0100

    Complete all button instances with the following colors

--- script.js
+++ script.js
@@ -26,21 +26,27 @@ let beats = {
     },
     "70": {
         beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+        button: new Button("#FF00FF",70)
     },
     "71": {
         beat: new Beat("./assets/Drum Snare Roll.mp3"),
+        button: new Button("#FF00FF",71)
     },
     "72": {
         beat: new Beat("./assets/PREL Musical 57.mp3"),
+        button: new Button("#FF00FF",72)
     },
     "74": {
         beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+        button: new Button("#FF00FF",74)
     },
     "75": {
         beat: new Beat("./assets/Musical Compos 33.mp3"),
+        button: new Button("#FFFFFF",75)
     },
     "76": {
         beat: new Beat("./assets/Musical Orches 4.mp3"),
+        button: new Button("#FFFFFF",76)
     }
 }
 
